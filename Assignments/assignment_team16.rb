0b4956c5cdd514b3c304fd2ce2fc7f042580ce66
dariooo512 <ferraidario@live.com>
2017-10-15 12:14:41 +0200

    inserimento numeri romani.

--- Assignments/assignment_team16.rb
+++ Assignments/assignment_team16.rb
@@ -1,6 +1,3 @@
-######
-# Implementa le seguenti funzioni come richiesto nel commento che le precede
-######
 
 # Restituisce una stringa che dice "Benvenuto"
 def welcome_message
@@ -30,48 +27,56 @@ def count_uppercase_letters(string)
   return string.scan(/[A-Z]/).length
 end
 
+# Associazione valori romani - decimali
+ROMAN_NUMBERS = {
+    "M" => 1000,
+    "CM" => 900,
+    "D" => 500,
+    "CD" => 400,
+    "C" => 100,
+    "XC" => 90,
+    "L" => 50,
+    "XL" => 40,
+    "X" => 10,
+    "IX" => 9,
+    "V" => 5,
+    "IV" => 4,
+    "I" => 1
+}
 
+# Conversione da decimale a numero romano
+def to_roman(n)
+  ROMAN_NUMBERS.map do |ltr, val|
+    amt, n = n.divmod(val)
+    ltr * amt
+  end.join
+end
 
-######
-# Completa l'implementazione delle seguenti classi seguendo quanto
-# richiesto dai commenti.
-######
 class Point2D
-  # la classe punto deve avere rendere accessibili gli attributi `x` e `y`
-  # IN SOLA LETTURA
   attr_reader :x, :y
 
-  # costruisce un punto con coordinate (x,y)
-  # nota che non e' necessario nessun controllo sul tipo di x e y
   def initialize(x, y)
     @x = x
     @y = y
   end
 
-  # la funzione `+` riceve come argomento un oggetto Point2D e restituisce un
-  # nuovo oggetto Point2D che ha come coordinate la somma delle coordinate dei
-  # due oggetti. (x1 + x2, y1 + y2)
-  # la funzione non deve alterare lo stato interno dell'oggetto, ma restituire
-  # un nuovo oggetto
+  # 
   def +(other)
     Point2D.new(@x + other.x, @y + other.y)
   end
 
   # Restituisce una rappresentazione testuale dell'oggetto punto, nella forma
   # "(x,y)", senza spazi.
-  # ES: siano x = 1, y = 2.345, la funzione deve restituire "(1,2.345)"
   def to_s
     "(#{@x},#{@y})"
   end
 end
 
-require 'date' # necessario per l'uso della classe Date
+require 'date'
 
 class Book
   attr_accessor :title, :author, :release_date, :publisher, :isbn
-
-  # Implementa il costruttore
-  # dai un'occhiata a https://robots.thoughtbot.com/ruby-2-keyword-arguments
+  
   def initialize(title:, author:, release_date:, publisher:, isbn:)
     @title = title
     @author = author
