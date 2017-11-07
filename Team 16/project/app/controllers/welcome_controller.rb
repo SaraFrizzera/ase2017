2af3a9fa66a1fafffdf1b30f408cc9c09e282756
dariooo512 <ferraidario@live.com>
2017-11-07 20:35:34 +0100

    API funzionante

--- Team 16/project/app/controllers/welcome_controller.rb
+++ Team 16/project/app/controllers/welcome_controller.rb
@@ -1,4 +1,18 @@
 class WelcomeController < ApplicationController
-  def index
+  def index; end
+
+  def test
+    puts 'aaaa'
+    user = User.new 'pippo'
+    render json: user
+  end
+  
+end
+
+class User
+  attr_reader :username
+
+  def initialize(username)
+    @username = username
   end
 end
--- Team 16/project/config/routes.rb
+++ Team 16/project/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-
-  get 'welcome/index'
-
-  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  root 'welcome#index'
+
+  get 'welcome/test'
+  root 'welcome#test'
 end
