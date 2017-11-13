602ca89fcb141613b956c3e3c2c25390e5e7a54f
FERRAIASUS500GB\ASUS <ferraidario@live.com>
2017-11-13 13:29:35 +0100

    non esiste method overloading in ruby -.-

--- Team 16/project/app/controllers/base_controller.rb
+++ Team 16/project/app/controllers/base_controller.rb
@@ -1,12 +1,7 @@
 class BaseController < ActionController::Base
-  def service_handler(service_function, headers)
+  def service_handler(service_function, headers = nil)
+    (render json: service_function.call; return) if headers.nil?
     render json: service_function.call(headers)
-  rescue ArgumentError => exception
-    render json: { error: exception.message }, status: :bad_request
-  end
-
-  def service_handler(service_function)
-    render json: service_function.call()
   rescue ArgumentError => exception
     render json: { error: exception.message }, status: :bad_request
   end
--- Team 16/project/config/routes.rb
+++ Team 16/project/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'welcome#index'
-  post 'user', to: 'user#create'
+  post 'user', to: 'user#create' # headers => username, password
   get 'user', to: 'user#find_specific' # headers => username
   get 'users', to: 'user#find_all' # headers =>
 end
