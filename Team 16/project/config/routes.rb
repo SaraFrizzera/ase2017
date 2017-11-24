75e002bca3d7c53306c691e8ac209d20be82e9c9
Dario Ferrai <ferraidario@live.com>
2017-11-24 13:53:49 +0100

    configuring routes

--- Team 16/project/config/routes.rb
+++ Team 16/project/config/routes.rb
@@ -1,9 +1,18 @@
-Rails.application.routes.draw do # dfault
+Rails.application.routes.draw do
   root to: 'welcome#index' # di defualt mi manda al welcome index
+
   post 'user', to: 'user#create' # headers => username, password. Post a user (localhost). "to:" richiama la classe usercontroller e la funzione create
   get 'user', to: 'user#find_specific' # headers => username.
   get 'users', to: 'user#find_all' # headers =>
-  post 'company', to: 'company#create'
-  post 'activity', to: 'activity#create'# headers => username, vatNumber, startTime, endTime
-  delete 'user', to: 'user#delete'
+  delete 'user', to: 'user#delete' # headers => username.
+
+  post 'company', to: 'company#create' # headers => companyName, vatNumber
+  get 'company', to: 'company#find_specific' # headers => companyName, vatNumber
+  get 'companies', to: 'company#find_all' # headers =>
+  delete 'company', to: 'company#delete' # headers => companyName, vatNumber
+
+  post 'activity', to: 'activity#create' # headers => username, vatNumber, startTime, endTime
+  get 'activity', to: 'activity#find_specific' # headers => username, vatNumber, startTime, endTime
+  get 'activities', to: 'activity#find_all' # headers =>
+  delete 'activity', to: 'activity#delete' # headers => username, vatNumber, startTime, endTime
 end
