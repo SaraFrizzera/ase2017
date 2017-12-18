4c02d04f375deb5531640ba00dcb25a48eb4667f
Dario Ferrai <ferraidario@live.com>
2017-12-18 17:58:40 +0100

    deve pulire l'aula.. minchia

--- Team 16/project/test/company/company_research_test.rb
+++ Team 16/project/test/company/company_research_test.rb
@@ -1,10 +1,27 @@
 class CompanyResearchTest < ActionDispatch::IntegrationTest
-  test 'Should fnd correct company when giving correct params' do
+  test 'Should find correct company when giving correct params' do
     reset_db
 
     # arrange
     creation_headers = { 'companyName' => 'RageDarioDevelopment', 'vatNumber' => '666' }
     research_headers = { 'companyName' => 'RageDarioDevelopment', 'vatNumber' => '666' }
+    created_company = CompanyService.new.create_company creation_headers
+
+    # act
+    company = CompanyService.new.find_company research_headers
+
+    # assert
+    assert_not_nil(company)
+    assert_equal(created_company, company)
+    assert_equal(creation_headers['companyName'], company.company_name)
+  end
+
+  test 'Should find correct company when giving correct id' do
+    reset_db
+
+    # arrange
+    creation_headers = { 'companyName' => 'RageDarioDevelopment', 'vatNumber' => '666' }
+    research_headers = { 'id' => '1' }
     created_company = CompanyService.new.create_company creation_headers
 
     # act
